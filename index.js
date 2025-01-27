f6b5155b3a0698bdb845f5c0963944e08a318732
Abanta6 <dalexbanta95@gmail.com>
2025-01-27 17:09:41 -0600

    Completed assignment

---
+++ index.js
@@ -0,0 +1,23 @@
+function convertToCelsius(f) {
+  return (f - 32) * (5 / 9);
+}
+
+function describeTemperature(f) {
+  const c = convertToCelsius(f);
+
+  let description = "";
+
+  if (c < 0) {
+    description = "very cold";
+  } else if (c < 20) {
+    description = "cold";
+  } else if (c < 30) {
+    description = "warm";
+  } else if (c < 40) {
+    description = "hot";
+  } else {
+    description = "very hot";
+  }
+
+  return `${f}°F is ${c}°C. That is ${description}`;
+}
